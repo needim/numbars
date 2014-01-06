4cd8926557a1052bc8d51eb2c167a9ed5073dbaf
Nedim Arabacı <nedimarabaci@gmail.com>
2014-01-06 09:17:40 +0200

    firefox support

--- src/numbars.js
+++ src/numbars.js
@@ -2,28 +2,50 @@ window.Numbars = function (element, num) {
 	num = num || 0;
 	this.element = element;
 	this.element.className += ' numbars';
-	this.max = parseInt(this.element.getAttribute('data-max'));
+	if (typeof this.element.attributes['data-max'] == 'undefined') {
+		this.max = this.element.getAttribute('data-max');
+	} else {
+		this.max = this.element.attributes['data-max'].nodeValue;
+	}
 	this.element.innerHTML = '<span class="numbars-progress"><span class="numbars-num">' + this.element.innerHTML + '</span></span>';
 	this.element.style.visibility = 'visible';
 
 	this.set = function (num) {
-		var that = this, o, currentNum = parseInt(that.element.firstChild.firstChild.innerText);
+		var that = this, o, currentNum;
+
+		if (that.element.firstChild.firstChild.innerText) {
+			currentNum = parseInt(that.element.firstChild.firstChild.innerText);
+		} else {
+			currentNum = parseInt(that.element.firstChild.firstChild.textContent);
+		}
 
 		if (num > that.max) num = that.max;
 		else if (num < 0) num = 0;
 
 		if (num > currentNum) {
 			currentNum++;
-			that.element.firstChild.firstChild.innerText = currentNum;
+			if (that.element.firstChild.firstChild.innerText) {
+				that.element.firstChild.firstChild.innerText = currentNum;
+			} else {
+				that.element.firstChild.firstChild.textContent = currentNum;
+			}
 			o = (currentNum / that.max) * 100;
 			this.element.firstChild.style.width = o + '%';
-			setTimeout(function () { that.set(num); }, 1);
+			setTimeout(function () {
+				that.set(num);
+			}, 1);
 		} else if (num < currentNum) {
 			currentNum--;
-			that.element.firstChild.firstChild.innerText = currentNum;
+			if (that.element.firstChild.firstChild.innerText) {
+				that.element.firstChild.firstChild.innerText = currentNum;
+			} else {
+				that.element.firstChild.firstChild.textContent = currentNum;
+			}
 			o = (currentNum / that.max) * 100;
 			this.element.firstChild.style.width = o + '%';
-			setTimeout(function () { that.set(num); }, 1);
+			setTimeout(function () {
+				that.set(num);
+			}, 1);
 		}
 
 		return this;
